af1f9ab9d3c8862a0fbfd39e860676bba9a1ace7
Ally Weir <allyjweir@github.com>
2022-11-21 11:06:34 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ echo "\n=========="
 echo "oh-my-zsh install"
 echo "=========="
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+export ZSH="${HOME}/.oh-my-zsh"
 
 # Add my custom additions to default zshrc
 echo "\n=========="
@@ -22,8 +23,14 @@ cat additions-to-zshrc >> $HOME/.zshrc
 
 # Install and configure asdf
 echo "\n=========="
-echo "Append to existing .zshrc"
+echo "Configure asdf"
 echo "=========="
 git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.10.2
 echo ". $HOME/.asdf/asdf.sh" >> $HOME/.zshrc
 echo ". $HOME/.asdf/completions/asdf.bash" >> $HOME/.zshrc
+
+# Final step: Source the updated `.zshrc` file
+echo "\n=========="
+echo "Source the updated .zshrc"
+echo "=========="
+source $HOME/.zshrc
