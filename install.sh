dffc92c44738bca169d9b1285874c582c4cc55dd
Ally Weir <allyjweir@github.com>
2022-11-21 10:49:36 +0000

    Fix paths for install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,29 @@
+echo "     ╔╗ ╔╗                        ╔╗          ╔╗     ╔╗  ╔═╗  ╔╗         "
+echo "     ║║ ║║       ╔╗               ║║          ║║    ╔╝╚╗ ║╔╝  ║║         "
+echo "╔══╗ ║║ ║║ ╔╗ ╔╗ ╚╝╔╗╔╗╔╗╔══╗╔╗╔═╗╚╝╔══╗    ╔═╝║╔══╗╚╗╔╝╔╝╚╗╔╗║║ ╔══╗╔══╗"
+echo "╚ ╗║ ║║ ║║ ║║ ║║ ╔╗║╚╝╚╝║║╔╗║╠╣║╔╝  ║══╣    ║╔╗║║╔╗║ ║║ ╚╗╔╝╠╣║║ ║╔╗║║══╣"
+echo "║╚╝╚╗║╚╗║╚╗║╚═╝║ ║║╚╗╔╗╔╝║║═╣║║║║   ╠══║    ║╚╝║║╚╝║ ║╚╗ ║║ ║║║╚╗║║═╣╠══║"
+echo "╚═══╝╚═╝╚═╝╚═╗╔╝ ║║ ╚╝╚╝ ╚══╝╚╝╚╝   ╚══╝    ╚══╝╚══╝ ╚═╝ ╚╝ ╚╝╚═╝╚══╝╚══╝"
+echo "           ╔═╝║ ╔╝║                                                      "
+echo "           ╚══╝ ╚═╝                                                      "
+echo "=========="
 
 # Install oh-my-zsh
+echo "\n=========="
+echo "oh-my-zsh install"
+echo "=========="
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Add my custom additions to default zshrc
-cat ~/additions-to-zshrc >> ~/.zshrc
+echo "\n=========="
+echo "Append to existing .zshrc"
+echo "=========="
+cat additions-to-zshrc >> $HOME/.zshrc
 
 # Install and configure asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
-echo ". $HOME/.asdf/asdf.sh" >> ~/.zshrc
-echo ". $HOME/.asdf/completions/asdf.bash" >> ~/.zshrc
+echo "\n=========="
+echo "Append to existing .zshrc"
+echo "=========="
+git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.10.2
+echo ". $HOME/.asdf/asdf.sh" >> $HOME/.zshrc
+echo ". $HOME/.asdf/completions/asdf.bash" >> $HOME/.zshrc
