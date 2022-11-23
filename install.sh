d045c3823479e5c8af061fea2a63d1778a88eddb
Ally Weir <allyjweir@github.com>
2022-11-23 11:09:23 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,6 +20,13 @@ echo "Copy my .zshrc file"
 echo "=========="
 cp .zshrc $HOME/.zshrc
 
+# Set my preferred shell
+# NOTE: This should be facilitated via VSCode when connecting to a Codespace but it doesn't seem to reliably pick up
+echo "\n=========="
+echo "Set zsh as preferred shell"
+echo "=========="
+chsh --shell /bin/zsh $(whoami)
+
 # Install and configure asdf
 echo "\n=========="
 echo "Configure asdf"
@@ -27,4 +34,3 @@ echo "=========="
 git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.10.2
 echo ". $HOME/.asdf/asdf.sh" >> $HOME/.zshrc
 echo ". $HOME/.asdf/completions/asdf.bash" >> $HOME/.zshrc
-
